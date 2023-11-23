a11d799b8907c819e938e7e9f066b069b7342a22
Joao Guilherme Barbosa <hello@joaoguilher.me>
2023-11-23 00:24:42 -0300

    adding avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@joaoguilher.me/core-ui'
+
+const meta = {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/joaoguilhermee.png',
+    alt: 'João Guilherme',
+  },
+} satisfies Meta<AvatarProps>
+export default meta
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
