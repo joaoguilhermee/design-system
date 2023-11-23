e567e3ee3c59a49b572ece0af71f238c450f7d98
Joao Guilherme Barbosa <hello@joaoguilher.me>
2023-11-23 01:13:32 -0300

    adding docs workflow

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -16,6 +16,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
@@ -24,5 +25,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
