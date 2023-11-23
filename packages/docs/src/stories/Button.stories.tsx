bcb69cac15888cae600d5baa18c607f692fa4cf7
Joao Guilherme Barbosa <hello@joaoguilher.me>
2023-11-23 00:32:57 -0300

    updating button

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,20 +1,51 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '@joaoguilher.me/core-ui'
+import { ArrowRight } from 'phosphor-react'
 
 const meta = {
   title: 'Form/Button',
   component: Button,
-
   args: {
-    children: 'Enviar',
+    children: 'Send',
   },
 } satisfies Meta<ButtonProps>
 export default meta
 
 export const Primary: StoryObj<ButtonProps> = {}
 
-export const Big: StoryObj<ButtonProps> = {
+export const Secondary: StoryObj<ButtonProps> = {
   args: {
-    size: 'big',
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next Step
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
   },
 }
