c18cbb1be26fb3df916d85d22d3a2268df9617a0
Joao Guilherme Barbosa <hello@joaoguilher.me>
2023-11-22 23:49:49 -0300

    renaming react to core-ui

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Button, ButtonProps } from '@joaoguilher.me/react'
+import { Button, ButtonProps } from '@joaoguilher.me/core-ui'
 
 export default {
   title: 'Form/Button',
